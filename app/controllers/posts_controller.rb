6d80046c5423d913571710717b9835b6745e56f3
arisa <arisankochira10@gmail.com>
2019-03-10 15:23:01 +0900

    更新の新しい順にindexを並べ替える

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
-      # 記事一覧用
-    @posts = Post.all
-  # 最新記事用
-    @new_posts = Post.all
+    @q = Post.order(created_at: :desc).ransack(params[:q])
+    @posts = @q.result(distinct: true)
+    @new_posts = Post.order(created_at: :desc).limit(5)
     @author = Author.first
   end
 
@@ -20,20 +20,31 @@ class PostsController < ApplicationController
     @post.save # saveをしてデータベースに保存する。
     redirect_to @post # showページにリダイレクト
   end
-
-  private
-
-  def post_params # ストロングパラメータを定義する
-    params.require(:post).permit(:title, :body, :category)
-  end
-
+  
   def edit
     @post = Post.find(params[:id])
   end
-
+  
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to @post
   end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  
+  def post_params # ストロングパラメータを定義する
+    params.require(:post).permit(:title, :body, :category)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
